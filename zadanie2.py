17d7cfaf9e041b6368325ea67b0f0875f28bc1d7
luczak79 <luczak79@googlemail.com>
2022-11-13 09:26:56 +0100

    ZADANIE2

--- zadanie2.py
+++ zadanie2.py
@@ -1,3 +1,15 @@
+# Proszę zbudować listę studentów składającą się z czterech osób, a następnie proszę:
+# •	Korzystając z funkcji append() dodać do listy kolejną osobę.
+# •	Korzystając z funkcji extend() dodać dwie osoby.
+# •	Posortować i wyświetlić alfabetycznie studentów, najpierw rosnąco, następnie malejąco.
+# •	Wydrukować na ekranie:
+# o	Drugiego studenta na liście.
+# o	Czwartego oraz piątego studenta.
+# o	Trzech ostatnich studentów.
+# •	Usunąć przedostatniego studenta na liście, korzystając z odpowiedniej funkcji i wyświetlić listę.
+# •	Utworzyć krotkę z finalnej wersji listy i ją wyświetlić.
+
+
 lista = ['Janek', 'Ola', 'Tomek', 'Marcin']
 
 lista.append('Janusz')
@@ -12,4 +24,5 @@ print(lista[3:5])
 print(lista[-3:])
 lista.remove(lista[-2])
 print(lista)
-print(tuple(lista))
+krotka = tuple(lista)
+print(krotka)
