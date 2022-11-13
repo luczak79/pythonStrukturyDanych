ebd560373afcf5f5c0802993dd4d91d332907377
luczak79 <luczak79@googlemail.com>
2022-11-13 10:03:05 +0100

    ZADANIE4

--- zadanie3.py
+++ zadanie3.py
@@ -3,13 +3,21 @@
 # •	Usunąć z utworzonej listy co drugi element zaczynając od piątego i ją wyświetlić.
 # •	Z finalnej wersji listy proszę wyliczyć średnią i ją wyświetlić.
 
-lista = []
-for i in range(3, 101):
-    if (i % 3 == 0):
-        lista.append(i)
+# lista = []
+# for i in range(3, 101):
+#     if (i % 3 == 0):
+#         lista.append(i)
+#
+# print(lista)
 
-print(lista)
+test_list = list(range(3,100,3))
+remove_list = test_list[5::2]
 
-lista2 = range(3,100,3)
-print(list(lista2))
+wynik = [i for i in test_list if i not in remove_list]
 
+print(wynik)
+
+del test_list[5::2]
+print(test_list)
+
+print(sum(test_list)/len(test_list))
